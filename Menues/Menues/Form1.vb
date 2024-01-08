d53b8d31bd21bef7e14c9401443710be2f9b0f09
Grzegorz Brzenczyszczykiewicz <mzopi744@learner.dvusd.org>
2024-01-08 14:02:29 -0700

    Made a menu on Visual Studio

--- Menues/Menues/Form1.vb
+++ Menues/Menues/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
 
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Me.Close()
+    End Sub
 End Class
